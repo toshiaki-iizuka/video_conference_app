a9274d58cbe501404cf88f0a2c47667c60ea9050
Toshiaki Iizuka <mog2to4aki@gmail.com>
2024-08-09 15:55:07 +0900

    feat: add clerk auth

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -29,10 +29,10 @@ const MobileNav = () => {
               src="/icons/logo.svg"
               width={32}
               height={32}
-              alt="Yoom logo"
+              alt="Poom logo"
               className="max-sm:size-10"
             />
-            <p className="text-[26px] font-extrabold text-white">Yoom</p>
+            <p className="text-[26px] font-extrabold text-white">Poom</p>
           </Link>
 
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import MobileNav from './MobileNav';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 
 const Navbar = () => {
   return (
@@ -12,14 +13,17 @@ const Navbar = () => {
           src="/icons/logo.svg"
           width={32}
           height={32}
-          alt="Yoom logo"
+          alt="Poom logo"
           className="max-sm:size-10"
         />
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">Yoom</p>
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">Poom</p>
       </Link>
 
       <div className="flex-between gap-5">
-        {/* Clerk - User Management */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
         <MobileNav />
       </div>
     </nav>
